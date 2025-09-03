e19a3b4f994b9d507790e688fb74fdd6dfc62439
Raginaakhter <raginaakhter052@gmail.com>
2025-09-04 00:43:57 +0600

    Add a button

--- src/Component/Auth/SignInForm.tsx
+++ src/Component/Auth/SignInForm.tsx
@@ -1,7 +1,7 @@
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-// import { Label } from "@/components/ui/label"
+// import { Label } from "@/components/ui/label
 // import { Switch } from "@/components/ui/switch"
 import { Card, CardContent } from "@/components/ui/card"
 import { Switch } from "@/components/ui/switch";
